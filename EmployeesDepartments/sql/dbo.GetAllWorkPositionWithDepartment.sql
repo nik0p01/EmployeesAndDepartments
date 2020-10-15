7512cdb24cc16ea07ae118a482f103b39656ef95
Николай Симонов <simnikola@gmail.com>
2020-10-15 15:59:07 +0300

    sp new

---
+++ EmployeesDepartments/sql/dbo.GetAllWorkPositionWithDepartment.sql
@@ -0,0 +1,15 @@
+﻿-- =============================================
+-- Author:		<Author,,Simonov NB>
+-- Create date: <Create Date,,11.10.2020>
+-- Description:	<Description,,Show list of departments with chief>
+-- =============================================
+CREATE PROCEDURE [GetAllWorkPositionWithDepartment]
+
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SELECT D.Name AS DepartmentName, WP.Name AS PosinionName, WR.Id
+	FROM dbo.Departments  as D,dbo.WorkingRates as WR, dbo.WorkPositions AS WP
+	WHERE     D.Id= WR.IdDepartment AND WP.Id = WR.IdWorkPosition
+
+END
